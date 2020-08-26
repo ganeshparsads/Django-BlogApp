2c73b55b3f74412ebc0092f4ec29a6457ee51340
Ganesh <ganesh@unbxd.com>
2020-08-26 21:05:41 +0530

    html template files

--- djangonapp/djangonapp/views.py
+++ djangonapp/djangonapp/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Home")
+    # return HttpResponse("Home")
+    return render(request, "home.html")
 
 
 def about(request):
-    return HttpResponse("About")
+    # return HttpResponse("About")
+    return render(request, "about.html")
